0fe691f8da15dbe4763fd6b7fa0e096d80642a80
jhorn26 <jeanfh26@gmail.com>
2022-10-10 08:31:26 -0300

    docstring

---
+++ docstring.py
@@ -0,0 +1,10 @@
+'''
+O inventário será representado como um dicionário, a key será o item e o value será a quantidade do produto.
+A função listar_inventário recebe um dicionário e printa os itens do inventário e suas respectivas quantidades, ela não retornará
+A função adiciona_item terá 3 parâmetros, o dicionário em que será adicionado o item, qual é o item (cujo o tipo é string) e a sua quantidade (cujo o tipo é int), 
+e retorna o dicionário com o item adicionado.
+A função remove_item terá 2 parâmetros, o dicionário em que será removido o item e qual o item que será removido (cujo o tipo é uma string), o item precisa existir no dicionário, senão dará um erro.
+A função busca_item terá 2 parâmetros, o dicionário em que será buscado o item e qual item será procurado (cujo o tipo é uma string), se o item não tiver no dicionário, retornará "item não encontrado".
+
+
+'''
